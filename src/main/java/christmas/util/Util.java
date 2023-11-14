47fb0c6ba2b0c85a34f6705d558b310acbb831ac
이병수 <qudtn0315@naver.com>
2023-11-14 15:49:37 +0900

    feat (Util) : 상수값 정리 클래스 생성

---
+++ src/main/java/christmas/util/Util.java
@@ -0,0 +1,17 @@
+package christmas.util;
+
+public class Util {
+    public static final int CHRISTMAS = 25;
+    public static final int ZERO = 0;
+    public static final int FIRST = 1;
+    public static final int SECOND = 2;
+    public static final int THIRD = 3;
+    public static final int SEVEN = 7;
+    public static final int HUNDRED = 100;
+    public static final int DISCOUNT_PRICE = 1000;
+
+    public static final int CAN_DISCOUNT = 10000;
+    public static final int MENU_DISCOUNT_PRICE = 2023;
+    public static final int CHAMPAGNE = 25000;
+    public static final int GIFT_BENEFIT_PRICE = 120000;
+}
